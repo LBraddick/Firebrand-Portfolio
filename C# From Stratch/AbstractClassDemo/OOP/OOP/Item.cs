333f6feca50b8397e24f2dea443d8a1b89bc3b1b
LBraddick <louisebraddick96@gmail.com>
2016-03-09 19:58:25 +0000

    Added files via upload

---
+++ C# From Stratch/AbstractClassDemo/OOP/OOP/Item.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP
+{
+    class Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
+        public virtual void Purchase ()
+        {
+            Console.WriteLine("Purchasing {0}", Name);
+        }
+    }
+
+    class Software : Item
+    {
+        public string ISBN { get; set; }
+        public override void Purchase()
+        {
+            Console.WriteLine("My ID is {0} and my ISBN is {1}",
+                ID, ISBN);
+        }
+    }
+
+    class Hardware : Item
+    {
+        public string SerialNumber { get; set; }
+    }
+
+    class Computer : Hardware
+    {
+        string CPUType { get; set; }
+        string Disks { get; set; }
+        public override void Purchase()
+        {
+            base.Purchase();
+            Console.WriteLine("myCPUType is {0}", CPUType);
+        }
+    }
+
+    class Peripheral : Hardware
+    {
+        string Decription { get; set; }
+    }
+}
