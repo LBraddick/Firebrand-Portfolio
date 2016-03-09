a10486a9e0d3570534b0458f3a2814ef476b0171
LBraddick <louisebraddick96@gmail.com>
2016-03-09 20:01:59 +0000

    Added files via upload

---
+++ C# From Stratch/AbstractClassDemo/EnumeratedConstants/EnumeratedConstants/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratedConstants
+{
+    class Program
+    {
+
+        enum Color
+        {
+            Red,
+            Orange,
+            Green,
+            Blue,
+            Indigo,
+            Violet
+        }
+
+        static void Main(string[] args)
+        {
+            Color favorite = Color.Green;
+
+            switch (favorite)
+            {
+                case Color.Red:
+                    Console.WriteLine("You choose Red");
+                    break;
+                case Color.Orange:
+                    Console.WriteLine("You choose Orange");
+                    break;
+                case Color.Green:
+                    Console.WriteLine("You choose Green");
+                    break;
+                case Color.Blue:
+                    Console.WriteLine("You choose Blue");
+                    break;
+                case Color.Indigo:
+                    Console.WriteLine("You choose Indigo");
+                    break;
+                case Color.Violet:
+                    Console.WriteLine("You choose Violet");
+                    break;
+                default:
+                    Console.WriteLine("You didn't choose a color");
+                    break;
+
+            }
+        }
+    }
+}
