92b8c0e143c86d9339bbaa250d0ea0ff55a1c1f8
LBraddick <louisebraddick96@gmail.com>
2016-03-09 20:07:55 +0000

    Added files via upload

---
+++ Automated Web Testing with Selenium/Test FrameWork/Test FrameWork/Pages.cs
@@ -0,0 +1,21 @@
+﻿namespace Test_FrameWork
+{
+    public static class Pages
+    {
+        public static class HomePage
+        {
+            static string Url = "https://www.pluralsight.com/";
+            private static string PageTitle = "Pluralsight - Hardcore Developer Training";
+
+            public static void Goto()
+            {
+                Browser.Goto(Url);
+            }
+
+            public static bool IsAt()
+            {
+                return Browser.Title == PageTitle;
+            }
+        }
+    }
+}
---
+++ Automated Web Testing with Selenium/Test FrameWork/Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test_FrameWork;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void Can_Go_To_HomePage()
+        {
+            Pages.HomePage.Goto();
+            Assert.IsTrue(Pages.HomePage.IsAt());
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Browser.Close();
+        }
+    }
+}
