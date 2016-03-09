7d21387f62466ba016aac1db9d6737350f2821f0
LBraddick <louisebraddick96@gmail.com>
2016-03-09 20:04:32 +0000

    Added files via upload

---
+++ C# From Stratch/AbstractClassDemo/CSSFSDemo3/CSSFSDemo3/Document.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSFSDemo3
+{
+    public class Document : Printable, Storable 
+    {
+        string DocName { get; set; }
+        public void CountCharacters()
+        {
+            Console.WriteLine("5,426 characters");
+        }
+
+        public int Status
+        {
+            get { return 0; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Writing document to Printer...");
+        }
+
+        public string Read()
+        {
+            Console.WriteLine("Getting Document from file...");
+            return "abc";
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Writing Document to file...");
+        }
+    }
+}
