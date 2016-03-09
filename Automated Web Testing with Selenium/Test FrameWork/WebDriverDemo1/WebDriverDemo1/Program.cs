2442968f1226aea6cda1d1003357e2606ede3adc
LBraddick <louisebraddick96@gmail.com>
2016-03-09 20:10:56 +0000

    Added files via upload

---
+++ Automated Web Testing with Selenium/Test FrameWork/WebDriverDemo1/WebDriverDemo1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+
+
+namespace WebDriverDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            //IWebDriver driver = new InternetExplorerDriver(@"C:\Users\louise.braddick\Documents\Libraries\");
+            //IWebDriver driver = new ChromeDriver(@"C:\Users\louise.braddick\Documents\Libraries\");
+            driver.Url = "http://www.google.com";
+
+            var searchBox = driver.FindElement(By.Id("lst-ib"));
+            searchBox.SendKeys("pluralsight");
+
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+
+            var imagesLink = driver.FindElements(By.ClassName("q qs"))[0];
+            imagesLink.Click();
+
+            var ul = driver.FindElement(By.ClassName("rg_di rg_el ivg-i"));
+            var firstImageLink = ul.FindElements(By.TagName("a"))[0];
+            firstImageLink.Click();
+
+        }
+    }
+}
+
