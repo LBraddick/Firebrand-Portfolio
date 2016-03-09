9f74ec90f295524494149f69df7a7e78ef56f044
LBraddick <louisebraddick96@gmail.com>
2016-03-09 20:03:54 +0000

    Added files via upload

---
+++ C# From Stratch/AbstractClassDemo/CSSFS2Demo/CSSFS2Demo/InsuranceInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSFS2Demo
+{
+    public class InsuranceInfo
+    {
+        public string PolicyName { get; set; }
+        public string PolicyId { get; set; }
+    }
+}
