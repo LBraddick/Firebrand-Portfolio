de47d7e2f5080c05e2773c56713e003e11a43070
LBraddick <louisebraddick96@gmail.com>
2016-03-09 20:05:35 +0000

    Added files via upload

---
+++ C# From Stratch/AbstractClassDemo/DictionaryDemo/DictionaryDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> captials = new Dictionary<string, string>();
+            //captials.Add("Alabama", "Montgomery");
+            //captials.Add("Alaska", "Juneau");
+            //captials.Add("Arizona", "Phoenix");
+            //captials.Add("Massachusetts", "Boston");
+            //captials.Add("Wyoming", "Cheyenne");
+
+            //string capitalOfMass = captials["Massachusetts"];
+            //Console.WriteLine("The captial of Masschusetts is {0}", capitalOfMass);
+
+            var theStates = State.GetStates();
+           // string capitalOfAlaska = theStates["Alaska"].Capital;
+            Console.WriteLine("The capital of Alaska is {0}, its population is {1} and it is {2} square miles",
+                theStates.capital, theStates.pop, theStates.size);
+
+            Console.ReadLine();
+        }
+    }
+}
