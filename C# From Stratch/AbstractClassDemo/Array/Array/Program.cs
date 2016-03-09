b1ffc95dfa6a82accb48d1e8676f45e923a9a487
LBraddick <louisebraddick96@gmail.com>
2016-03-09 19:53:51 +0000

    Added files via upload

---
+++ C# From Stratch/AbstractClassDemo/Array/Array/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    var items = new Item[5];
+            //    Random r = new Random();
+            //    for (int i=0; i < items.Length; i++)
+            //    {
+            //        items[i] = new Item(r.Next());
+            //    }
+
+            //    Console.WriteLine("Items: ");
+
+            //    foreach (Item item in items)
+            //    {
+            //        Console.WriteLine("ItemID = {0}", item.ID);
+            //    }
+
+            //    Item myItem = items[1];
+            //    Console.WriteLine("ID of second item = {0}", myItem.ID);
+
+            //    Console.ReadLine();
+
+            var Authors = new string[5];
+            Authors[0] = "Failkner";
+            Authors[1] = "Proust";
+            Authors[2] = "Stephenson";
+            Authors[3] = "Gibson";
+            Authors[4] = "Melville";
+
+            Console.WriteLine("Authors .....");
+            foreach (string s in Authors)
+            {
+                Console.WriteLine(s);
+            }
+
+            Array.Sort(Authors);
+
+            Console.WriteLine("Authors .....");
+            foreach (string s in Authors)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
